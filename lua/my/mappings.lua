817be7725761f099d251ada0dd9cb49be97bcbef
Тигран Ростомян <tigranrostoman@MacBook-Pro-Tigran.local>
2023-09-10 20:35:51 +0200

    ref: vim.keymap.set => map

--- lua/my/mappings.lua
+++ lua/my/mappings.lua
@@ -1,26 +1,28 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex) -- Jump to file tree
-vim.keymap.set('n', ';', vim.cmd.noh) -- Cancel search highlighting
+local map = vim.keymap.set
 
-vim.keymap.set('n', 'gp', function() vim.cmd("b#") end) -- Jump to previous buffer
+vim.g.mapleader = " "
+map("n", "<leader>e", vim.cmd.Ex) -- Jump to file tree
+map('n', ';', vim.cmd.noh) -- Cancel search highlighting
+
+map('n', 'gp', function() vim.cmd("b#") end) -- Jump to previous buffer
 
 -- Fast escape from normal mode
-vim.keymap.set('i', 'jk', function () vim.cmd("stopinsert") end)
+map('i', 'jk', function () vim.cmd("stopinsert") end)
 
 -- Move selected lines
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Centered half-page move
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+map("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+map({"n", "v"}, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set("n", "<leader>s{", [[@s]])
+map("n", "<leader>s{", [[@s]])
 vim.cmd [[source ~/.config/nvim/lua/my/mappings.vim]]
