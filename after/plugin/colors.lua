ee4906bd0dc71cee9e15eb129d12699668400910
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-11-30 13:10:42 +0200

    fix: removed auto dark mode

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,15 +1,3 @@
 vim.cmd.colorscheme('everforest')
 
-local auto_dark_mode = require('auto-dark-mode')
-auto_dark_mode.setup({
-  update_interval = 1000,
-  -- Dark mode options
-  set_dark_mode = function()
-    vim.api.nvim_set_option('background', 'dark')
-  end,
-  -- Dark mode options
-  set_light_mode = function()
-    vim.api.nvim_set_option('background', 'light')
-  end,
-})
-auto_dark_mode.init()
+
