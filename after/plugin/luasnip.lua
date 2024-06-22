6869f81d33d49a0fc48806d5576dc9bd9955c191
rostomyan.tigran <rostomyan.tigran@wb.ru>
2024-06-22 12:52:32 +0200

    new snippets

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -39,146 +39,11 @@ local ts_utils = require "nvim-treesitter.ts_utils"
 
 local get_node_text = vim.treesitter.get_node_text
 
-local transforms = {
-  int = function(_, _)
-    return t "0"
-  end,
-
-  bool = function(_, _)
-    return t "false"
-  end,
-
-  string = function(_, _)
-    return t [[""]]
-  end,
-
-  error = function(_, info)
-    if info then
-      info.index = info.index + 1
-
-      return c(info.index, {
-        t(info.err_name),
-        -- fmt('errors.Wrap("{message}")', { message = i(6, "message") }),
-        t(string.format('errors.Wrap(%s, "%s")', info.err_name, info.func_name)),
-      })
-    else
-      return t "err"
-    end
-  end,
-
-  -- Types with a "*" mean they are pointers, so return nil
-  [function(text)
-    return string.find(text, "*", 1, true) ~= nil
-  end] = function(_, _)
-    return t "nil"
-  end,
-}
-
-local transform = function(text, info)
-  local condition_matches = function(condition, ...)
-    if type(condition) == "string" then
-      return condition == text
-    else
-      return condition(...)
-    end
-  end
-
-  for condition, result in pairs(transforms) do
-    if condition_matches(condition, text, info) then
-      return result(text, info)
-    end
-  end
-
-  return t(text)
-end
-
-local handlers = {
-  parameter_list = function(node, info)
-    local result = {}
-
-    local count = node:named_child_count()
-    for idx = 0, count - 1 do
-      local matching_node = node:named_child(idx)
-      local type_node = matching_node:field("type")[1]
-      table.insert(result, transform(get_node_text(type_node, 0), info))
-      if idx ~= count - 1 then
-        table.insert(result, t { ", " })
-      end
-    end
-
-    return result
-  end,
-
-  type_identifier = function(node, info)
-    local text = get_node_text(node, 0)
-    return { transform(text, info) }
-  end,
-}
-
-local function_node_types = {
-  function_declaration = true,
-  method_declaration = true,
-  func_literal = true,
-}
-
-local function go_result_type(info)
-  local cursor_node = ts_utils.get_node_at_cursor()
-  local scope = ts_locals.get_scope_tree(cursor_node, 0)
-
-  local function_node
-  for _, v in ipairs(scope) do
-    if function_node_types[v:type()] then
-      function_node = v
-      break
-    end
-  end
-
-  if not function_node then
-    print "Not inside of a function"
-    return t ""
-  end
-
-  local query = vim.treesitter.query.parse(
-    "go",
-    [[
-      [
-        (method_declaration result: (_) @id)
-        (function_declaration result: (_) @id)
-        (func_literal result: (_) @id)
-      ]
-    ]]
-  )
-  for _, node in query:iter_captures(function_node, 0) do
-    if handlers[node:type()] then
-      return handlers[node:type()](node, info)
-    end
-  end
-end
-
-local go_ret_vals = function(args)
-  return snippet_from_nodes(
-    nil,
-    go_result_type {
-      index = 0,
-      err_name = args[1][1],
-      func_name = args[2][1],
-    }
-  )
-end
-
-local go_ret_vals_now_func = function(args)
-  return snippet_from_nodes(
-    nil,
-    go_result_type {
-      index = 0,
-      err_name = args[1][1],
-      func_name = "func()",
-    }
-  )
-end
-
 -------------------------------------------------------------------------------
 --------------------------------- SNIPPETS ------------------------------------
+-------------------------------------------------------------------------------
+
+----------------------------------- ODIN --------------------------------------
 -------------------------------------------------------------------------------
 
 ls.add_snippets("odin", {
@@ -197,20 +62,14 @@ ls.add_snippets("odin", {
 
 })
 
+------------------------------------ GO ---------------------------------------
+-------------------------------------------------------------------------------
+
 ls.add_snippets("go", {
   s("trig", c(1, {
     t("Ugh boring, a text node"),
     i(nil, "At least I can edit something now..."),
     f(function(args) return "Still only counts as text!!" end, {})
-  })),
-
-  s("v", fmta("<val>, <err> := <fn>\nif <err_rep> != nil {\n\treturn <ret>\n}\n<final>", {
-    val = i(1, "val"),
-    err = i(2, "err"),
-    fn = i(3, "fn"),
-    ret = d(4, go_ret_vals, { 2, 3 }),
-    err_rep = rep(2),
-    final = i(0)
   })),
 
   -- s("i", fmta("if <err> != nil {\n\treturn <ret>\n}\n<final>", {
@@ -377,6 +236,20 @@ if err != nil {
     })),
 
   s("c", fmta("ctx context.Context<cur>", {
+    cur = i(0),
+  })),
+
+  s("fc", fmta("func(ctx context.Context) error<cur>", {
+    cur = i(0),
+  })),
+
+  s("ni", fmta("errors.New(\"not implemented\")<cur>", {
+    cur = i(0),
+  })),
+
+
+  s("mck", fmta("//go:generate mockery --dir=. --name=<name> --exported --with-expecter<cur>", {
+    name = i(1, "name"),
     cur = i(0),
   })),
 })
