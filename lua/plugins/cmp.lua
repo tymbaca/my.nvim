4e617c0c9a59d37dbce930401faa467ad88af2e4
rostomyan.tigran <rostomyan.tigran@wb.ru>
2024-08-25 01:31:26 +0200

    telescope works...

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,5 @@
 return {
 	"hrsh7th/nvim-cmp",
-	-- event = "InsertEnter",
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
