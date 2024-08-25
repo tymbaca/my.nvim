e0d53594e69e89fd0fbcf9502fccfe935d32ee72
rostomyan.tigran <rostomyan.tigran@wb.ru>
2024-08-25 12:39:18 +0200

    more!!!

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -3,19 +3,27 @@ return {
   cmd = { "BlameToggle" },
   config = function()
     require("blame").setup({
-      -- date_format = "%d.%m.%Y",
-      -- virtual_style = "right",
-      -- merge_consecutive = false,
-      -- max_summary_width = 30,
-      -- colors = nil,
-      -- commit_detail_view = "vsplit",
-      -- mappings = {
-      --   commit_info = "K",
-      --   stack_push = "<TAB>",
-      --   stack_pop = "<BS>",
-      --   show_commit = "<CR>",
-      --   close = { "<esc>", "q" },
-      -- }
+      date_format = "%d.%m.%Y",
+      virtual_style = "right_align",
+      views = {
+        window = window_view,
+        virtual = virtual_view,
+        default = window_view,
+      },
+      focus_blame = true,
+      merge_consecutive = false,
+      max_summary_width = 30,
+      colors = nil,
+      blame_options = nil,
+      commit_detail_view = "vsplit",
+      format_fn = require("blame.formats.default_formats").date_message,
+      mappings = {
+        commit_info = "K",
+        stack_push = "<TAB>",
+        stack_pop = "<BS>",
+        show_commit = "<CR>",
+        close = { "<esc>", "q" },
+      }
     })
   end,
   -- opts = {
@@ -34,7 +42,7 @@ return {
   --   }
   -- },
   keys = {
-    -- { "<leader>b", "<cmd>BlameToggle window<cr>" },
-    { "<leader>b", "<cmd>BlameToggle virual<cr>" },
+    { "<leader>b", "<cmd>BlameToggle window<cr>" },
+    -- { "<leader>b", "<cmd>BlameToggle virtual<cr>" },
   },
 }
---
+++ lua/plugins/marks.lua
@@ -0,0 +1,15 @@
+return {
+  'chentoast/marks.nvim',
+  config = function()
+    require 'marks'.setup {
+      -- whether to map keybinds or not. default true
+      default_mappings = false,
+      mappings = {
+        toggle = "mm",
+        next = "]m",
+        delete_buf = "dam",
+        prev = "[m", -- pass false to disable only this default mapping
+      },
+    }
+  end
+}
