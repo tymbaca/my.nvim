ecb3dfba344e4f6a09c0925044685e8ff574e7e5
Tigran Rostomyan <tumb4ka228@gmail.com>
2025-03-27 22:44:37 +0300

    devil

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -17,6 +17,8 @@ map("v", "K", ":m '<-2<CR>gv=gv")
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 
+map('i', '<C-c>', "<esc>")
+
 -- greatest remap ever
 map("x", "<leader>p", [["_dP]])
 
